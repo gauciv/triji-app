c9d0b74896567110ef65f4842b8a89816267b3a8
gauciv <vincentaugusto16@gmail.com>
2025-07-05 12:45:02 +0800

    refactor(app): set splash screen as initial app view

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import LoginScreen from './LoginScreen';
-import RegisterScreen from './RegisterScreen';
-import VerificationScreen from './VerificationScreen';
-import DashboardScreen from './DashboardScreen';
-
-const Stack = createStackNavigator();
+import SplashScreen from './SplashScreen';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Verification" component={VerificationScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-      </Stack.Navigator>
+    <>
+      <SplashScreen />
       <StatusBar style="light" />
-    </NavigationContainer>
+    </>
   );
 }
